153c1ec93ce4a0eaef4e1ed7d67ccdfb3717a677
waveRider2069 <37651820+waveRider2069@users.noreply.github.com>
2019-04-19 12:08:46 +0800

    Update subject.py

--- subject.py
+++ subject.py
@@ -9,7 +9,7 @@ from datetime import date, timedelta, datetime
 
 bp = Blueprint('subject', __name__, template_folder='../templates', url_prefix='/subject')
 # add some change
-
+# add second change
 @bp.route('/new')
 @login_required
 def new():
